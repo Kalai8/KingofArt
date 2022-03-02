bc563d8ce985382996e8e16c5f6a63aa94ea639a
Kalai8 <kalairaja1994@gmail.com>
2022-03-02 11:43:56 +0530

    New1

---
+++ src/org/java/JavaClass.java
@@ -0,0 +1,17 @@
+package org.java;
+//how to create the object for call method name
+public class JavaClass {
+	
+	private void stafflecture() {
+System.out.println("Karthik");
+	}
+	private void dayOne() {
+System.out.println("How to Create Object and get Output");
+	}
+	public static void main(String[] args) {
+		JavaClass j = new JavaClass();
+		j.stafflecture();
+		j.dayOne();
+	}
+
+}
